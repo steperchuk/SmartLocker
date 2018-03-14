653570165e5e058673a1f08df692b4175319e0c2
Sergey Teperchuk <sergey.teperchuk@mcdean.com>
2018-03-14 22:55:18 +0200

    Implement stars rating

--- app/src/main/java/me/sergeyteperchuk/lockscreen/StatisticsActivity.java
+++ app/src/main/java/me/sergeyteperchuk/lockscreen/StatisticsActivity.java
@@ -16,7 +16,8 @@ public class StatisticsActivity extends Activity {
     List<Drawable> images = new ArrayList<>();
     List<Integer> answersCount = new ArrayList<>();
     List<Integer> correctAnswersCount = new ArrayList<>();
-    List<String> statistics = new ArrayList<>(); //{"12 из 157", "2 из 34", "3 из 56"};
+    List<String> statistics = new ArrayList<>();
+    List<Integer> progress = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +35,21 @@ public class StatisticsActivity extends Activity {
 
         for(int i = 0 ; i < subjects.size(); i++){
             statistics.add(correctAnswersCount.get(i) + " из " + answersCount.get(i));
+            progress.add(getRating(correctAnswersCount.get(i), answersCount.get(i)));
         }
 
-        StatisticsListAdapter adapter=new StatisticsListAdapter(this, subjects, images, statistics);
+        StatisticsListAdapter adapter=new StatisticsListAdapter(this, subjects, images, statistics, progress);
         list.setAdapter(adapter);
+    }
 
+    private int getRating(Integer answersCount, Integer totalQuestions){
+        int percent = (answersCount * 100) / totalQuestions;
+        if (percent > 0 && percent< 20){return 1;}
+        if (percent > 20 && percent < 40){return 2;}
+        if (percent > 40 && percent < 60){return 3;}
+        if (percent > 60 && percent < 80){return 4;}
+        if (percent == 100){return 5;}
+
+        return 0;
     }
 }
--- app/src/main/java/me/sergeyteperchuk/lockscreen/StatisticsListAdapter.java
+++ app/src/main/java/me/sergeyteperchuk/lockscreen/StatisticsListAdapter.java
@@ -1,12 +1,15 @@
 package me.sergeyteperchuk.lockscreen;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,14 +20,16 @@ public class StatisticsListAdapter extends ArrayAdapter<String> {
     private final List<String> subject;
     private final List<Drawable> image;
     private final List<String> statistics;
+    private final List<Integer> progress;
 
-    public StatisticsListAdapter(Activity context, List<String> subjects, List<Drawable> image, List<String> statistics) {
+    public StatisticsListAdapter(Activity context, List<String> subjects, List<Drawable> image, List<String> statistics, List<Integer> progress) {
         super(context, R.layout.statistics_list_item, subjects);
 
         this.context = context;
         this.subject = subjects;
         this.image = image;
         this.statistics = statistics;
+        this.progress = progress;
     }
 
     public View getView(int position, View view, ViewGroup parent) {
@@ -34,10 +39,13 @@ public class StatisticsListAdapter extends ArrayAdapter<String> {
         TextView subjectTxt = (TextView) rowView.findViewById(R.id.subject);
         TextView statisticsTxt = (TextView) rowView.findViewById(R.id.statistic);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        RatingBar ratingBar = (RatingBar) rowView.findViewById(R.id.myRatingBar);
+        ratingBar.setMax(5);
 
         subjectTxt.setText(this.subject.get(position));
         statisticsTxt.setText(this.statistics.get(position));
         imageView.setImageDrawable(this.image.get(position));
+        ratingBar.setProgress(this.progress.get(position));
         return rowView;
     };
 }
