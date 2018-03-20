a905e71c3c66a46065a9726e64e671d6c6308258
Sergey Teperchuk <sergey.teperchuk@mcdean.com>
2018-03-20 22:35:37 +0200

    Fix bootload

--- app/src/main/java/com/sergeyteperchuk/lockscreen/utils/MyBroadcastReceiver.java
+++ app/src/main/java/com/sergeyteperchuk/lockscreen/utils/MyBroadcastReceiver.java
@@ -3,11 +3,21 @@ package com.sergeyteperchuk.lockscreen.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent startServiceIntent = new Intent(context, LockscreenService.class);
+        if(!getServiceState(context)){
+            return;
+        }
         context.startService(startServiceIntent);
+    }
+
+
+    private boolean getServiceState(Context context){
+        SharedPreferences sharedPref = context.getSharedPreferences("Preferences",Context.MODE_PRIVATE);
+        return sharedPref.getBoolean("serviceState", false);
     }
 }
